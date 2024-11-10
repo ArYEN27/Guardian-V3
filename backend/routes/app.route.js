3590f62c8408d731079ad3b661d046b2322b0cab
Aryan Rajeev <raghavaan27@gmail.com>
2024-11-11 05:05:41 +0530

    Added User verification in API endpoint-Sign In

--- backend/routes/app.route.js
+++ backend/routes/app.route.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { User, History } from "../db_models/index.js";
-import Op from "sequelize"
 
 const router = express.Router();
 
@@ -27,44 +26,33 @@ router.post("/signup", async (req, res) => {
 router.post("/signin", async (req, res) => {
   try {
 
-    const { emailId } = req.body;
+    const { emailId, password } = req.body;
 
-    if (!emailId) {
+    if (!emailId || !password) {
       return res.status(400).json({ message: 'Email & Password are required' });
     }
 
-    console.log('Received emailId:', emailId);
-
     const currUser = await User.findOne({ where: { emailId: `${emailId}` } });
-
     console.log('Found user:', currUser);
-    // console.log('User dataValues:', currUser ? currUser.dataValues : 'No user found');
 
 
-    if (currUser) {
-      // Return the user details if found
-      res.json({ message: 'User found', user: currUser.dataValues });
-    } else {
-      // Return a 404 status if the user is not found
-      res.status(404).json({ message: 'User not found' });
+    if (!currUser) {
+      res.json({ message: 'User not found' });
+    }
+    
+    if (currUser.password === password) {
+      return res.json({ message: "User verified successfully", currUser });
+    }
+    else {
+      res.status(404).json({ message: 'Invalid Password' });
     }
   } catch (error) {
-    console.error('Error fetching user:', error.message);
+    console.error('Error verifying user:', error.message);
     res.status(500).json({ message: 'Internal server error' });
   }
 });
 
-    //   // const {  emailId, password } = req.body;
-  
-    //   // const user = await User.find({
-    //   //   emailId,
-    //   //   password,
-    //   // });
-  
-    //   res.status(201).json({ message: "User found", data: `${user.email} & ${user.password}` });
-    // } catch (error) {
-    //   res.status(500).json({ message: "Error creating user", error: error.message });
-    
+// Test    
 router.get("/users", async (req, res) => {
     try {
       const users = await User.findAll();
